114885c5d24a4634f428f22a7d04ef14e15dffb1
Aliyu Sudais <Asudaiz01@gmail.com>
2022-09-10 10:09:01 -0700

    investor vibes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,13 @@
 /**
  * main - print the string in the put function
  *
+ * Description: using the main function, 
+ * this program will print "Programming is like building a multilingual puzzle
  *
-Description: using the main function
- *
- this program will print "Programming is like building a multilingual puzzle
- *Return: 0
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
